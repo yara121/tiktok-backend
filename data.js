ded23de3899e5d664700d8b132b80746f5ca51ef
yara <yaraeb121@gmail.com>
2020-12-30 02:24:13 +0200

    tiktok server commit

---
+++ data.js
@@ -0,0 +1,21 @@
+const data = [
+    {
+        url:"https://www.youtube.com/watch?v=nJBxgNFvV50" ,
+        channel:"eetttyyy",
+        description : "Yoo this works",
+        song:"99 problems but REACT AINT ONE!",
+        likes :"123",
+        messages:"456",
+        shares:"789"
+    },
+    {
+        url:"https://www.youtube.com/watch?v=nJBxgNFvV50" ,
+        channel:"gggghhhhmmmm",
+        description : "Y good job",
+        song:"99 problems but REACT AINT ONE!",
+        likes :"23",
+        messages:"976",
+        shares:"234"
+    }
+]
+export default data
